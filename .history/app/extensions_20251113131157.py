9d82bb70a81e93d63c193188329fe35dc1c63fa6
Ibrokhim <ibroximmusaxodjaeav@gmail.com>
2026-02-21 01:56:04 +0000

    Remove generated files and uploads from repo

--- .history/app/extensions_20251113131157.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-
-# where to redirect if user is not logged in
-login_manager.login_view = "auth.login"
--- .history/app/routes/__init___20251113133818.py
+++
@@ -1,2 +0,0 @@
-# This file makes "app.routes" a Python package.
-# We don't need to import any blueprints here yet.
